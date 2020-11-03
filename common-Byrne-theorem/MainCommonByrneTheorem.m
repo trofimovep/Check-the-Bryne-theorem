21094620cb60ceb69334abe9b0ec154c2f4553c7
trofimovep <trofimovep42@gmail.com>
2020-11-03 17:22:51 +0300

    new

--- common-Byrne-theorem/MainCommonByrneTheorem.m
+++ common-Byrne-theorem/MainCommonByrneTheorem.m
@@ -3,19 +3,20 @@ clear; clc;
 inLinear = 0;
 outOfLinear = 0;
 for n = 2:2
-    for k = 3:7
+    for k = 2:2
         m = n + k;
         disp("Number of variables: " + n);
         disp("Number of Equation: " + m);
-        [system, rightPart] = makeSystem(m, n);
+        [system, rightPart] = makeSystem(m, n);        
         [normSystem, normRightPart] = normilizeMatrixRowsAndRightPart(system, rightPart);
+        gpsol = pinv(normSystem) * normRightPart; % geometrical pseudosolution
+        
         permutations = arranges(m, n+1);
         [permutationsVars, ~] = size(permutations);
         disp("Permutations variants: " + permutationsVars);
         
-        if permutationsVars > 1
-            gpsol = pinv(normSystem) * normRightPart; % geometrical pseudosolution
-            centers = zeros(permutationsVars, n);           
+        centers = zeros(permutationsVars, n);  
+        if permutationsVars > 1         
             currentSubSystem = zeros(n+1, n);
             currentSubRightPart = zeros(n+1, 1);
             for var = 1:permutationsVars
@@ -27,20 +28,43 @@ for n = 2:2
                 centers(var, :) = currentGeomPseudoSol;
             end
             
-         if VectorInConvHull(centers, gpsol')
-            inLinear = inLinear  + 1;
-            disp('--- For this in a conv hull ---');
-         else
-             outOfLinear = outOfLinear + 1;
-             disp('--- For this NOT in a conv hull ---');
-         end
-         disp('================================================')
+            if VectorInConvHull(centers, gpsol')
+                inLinear = inLinear  + 1;
+                disp('--- For this in a conv hull ---');
+            else
+                 outOfLinear = outOfLinear + 1;
+                disp('--- For this NOT in a conv hull ---');
+            end
+         disp('================================================')               
         end
+         % Plotting part
+         if n == 2 && k == 2
+             x=linspace(-1.1, 1.1);
+             for i = 1:4
+                y = (-system(i,1)*x + rightPart(i)) / system(i,2);
+                plot(x, y, 'LineWidth', 1.5);
+                tl = texlabel("y=(" + -system(i,1)+"x+" + rightPart(i)+")/" +system(i,2));
+                text(x(i), y(i), tl,'FontSize', 7);
+                hold on;
+             end
+             plot(gpsol(1),gpsol(2),'o','Color','r','MarkerEdgeColor','k',...
+                 'MarkerFaceColor','r', 'MarkerSize',6);
+             hold on;
+             
+             for i=1:size(centers)
+                 plot(centers(i, 1),centers(i, 2),'+','Color', 'magenta');
+                 hold on;
+             end
+            hold off; 
+         end
     end
 end
 
 disp("In total in conv hull: " + inLinear);
 disp("In total NOT in conv hull: " + outOfLinear);
+
+
+
 
 
 
--- common-Byrne-theorem/makeSystem.m
+++ common-Byrne-theorem/makeSystem.m
@@ -1,8 +1,13 @@
 function [system, rightpart] = makeSystem(m,n)
 % This function makes a system AX = B with random coefficient
 % where n - variable amount, m - amount of equation  
-system = rand(m, n);
-rightpart = rand(m , 1);
+
+% system = randi(m, n);
+% rightpart = rand(m , 1);
+
+system = randi([-10, 10], m, n);
+rightpart = randi([-10, 10], m, 1);
+
 % disp('Current system: '); disp(system);
 % disp('Right part:'); disp(rightpart);
 end
