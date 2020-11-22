d1e27ce1821f8a7bb45710a2e4ac6e7ff8f65699
trofimovep <trofimovep42@gmail.com>
2020-11-22 17:19:25 +0300

    new

--- order dependency/MainOrderDependency.m
+++ order dependency/MainOrderDependency.m
@@ -10,25 +10,45 @@ permutations = perms(1:m);
 [permutationsVar, ~] = size(permutations);
 disp("Permutation amount: " + permutationsVar);
 
-x = linspace(-3, 3);
+x = linspace(-10, 10);
 for i = 1:m
     y = (-system(i,1)*x + rightPart(i)) / system(i,2);
-    plot(x, y, 'LineWidth', 1.5);
+    plot(x, y, 'LineWidth', 1.5, 'color', 'black');
     tl = texlabel('I');
     text(x(i), y(i), tl,'FontSize', 7);
     hold on;
 end
-drawLimitCycle(normSystem, normRightPart);
 
 
+colors = ['r', 'b', 'g'];
+legends = ['1-2-3 order', '1-3-2 order', '3-1-2 order'];
+% 1 2 3 order
+drawLimitCycle(normSystem, normRightPart,colors(1));
+hold on;
 
-% function B = TEMP(A, m, n);
-% B = A;
-% buff = B(m, :);
-% B(m,:) = B(n,:);
-% B(n,:) = buff;
+% 1 3 2 order
+system132 = normSystem;
+systemBuff = system132(3,:);
+system132(3,:) = system132(2,:);
+system132(2,:) = systemBuff;
 
-for i=1:permutationsVar
-end
+rightPart132 = rightPart;
+rightPartBuff = rightPart132(3);
+rightPart132(3) = rightPart132(2);
+rightPart132(2) = rightPartBuff;
+
+drawLimitCycle(system132, rightPart132,colors(2));
+
+% 3 1 2 order
+system312 = system132;
+buff = system312(1,:);
+system312(1,:) = system312(2,:);
+system312(2,:) = buff;
+
+rightPart312 = rightPart132;
+rightPartBuff = rightPart312(1);
+rightPart312(1) = rightPart312(2);
+rightPart312(2) = rightPartBuff;
+drawLimitCycle(system312, rightPart312, colors(3));
 
 
--- order dependency/drawLimitCycle.m
+++ order dependency/drawLimitCycle.m
@@ -1,4 +1,4 @@
-function [] = drawLimitCycle(system, rightPart)
+function [p] = drawLimitCycle(system, rightPart, color)
 %DRAWLIMITCYCLE Draws a limit cycle of a Kachmarz algorithm
 % NB!! needs a kacmarz.m function 
 [m,n] = size(system);
@@ -8,8 +8,8 @@ else
     limitCycle = kachmarz(system, rightPart);
     limitCycle = [limitCycle; limitCycle(1,:)];
     xpoints = limitCycle(:, 1); 
-    ypoints = limitCycle(:, 2);
-    plot(xpoints, ypoints, 'Color', 'black');
+    ypoints = limitCycle(:, 2);    
+    plot(xpoints, ypoints, 'color', color);
     hold on;
 end    
 end
